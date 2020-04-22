fdd1e27ed202212e0abbfe3bd8ddf51d9d31295f
Oleg Kurbatov <o.v.kurbatov@gmail.com>
2020-04-22 19:42:46 +0300

    doc(core): add doc for Command and Query protocols

--- src/norm/core.clj
+++ src/norm/core.clj
@@ -5,19 +5,139 @@
 (defprotocol Command
   "A command to an abstract storage."
   :extend-via-metadata true
-  (execute [command] "Executes the `command` returning an execution result.")
-  (then ^Command [command next-command] "Combines two commands for execution in a transaction."))
+  (execute [command]
+    "Executes the `command` returning an execution result.
+
+    Example:
+
+    ```
+    (execute delete-inactive-users)
+    ```")
+  (then ^Command [command next-command]
+    "Combines two commands for execution in a transaction.
+
+    Execution returns a vector containing the results of execution
+    of each particular command.
+
+    If an exception occurs during the execution, the whole chain
+    of commands is rolled back.
+
+    Example:
+
+    ```
+    ; execute all commands in a transaction
+    (-> first-command
+        (then following-command)
+        (then final-command)
+        execute)
+    ```"))
 
 (defprotocol Query
   "A query to an abstract storage."
   :extend-via-metadata true
-  (join ^Query [query op source clause] "Adds `source` to the query linking it with specified `op`eration.")
-  (where ^Query [query clauses] "Adds clauses to the query.")
-  (order ^Query [query order] "Adds or replaces the order of records of the query result.")
-  (skip ^Query [query amount] "Skips specified amount of records of the result.")
-  (limit ^Query [query amount] "Limits the max number of records in the result.")
-  (fetch [query] [query fields] "Fetches the data that comply to the query.")
-  (fetch-count [query] "Fetches the amount of records for the given query."))
+  (join ^Query [query op source clause]
+    "Adds `source` to the query linking it with specified `op`eration.
+
+    Example:
+
+    ```
+    (-> users-query
+        (join :left-join :people {:users/person-id :people/id})
+        (where {:people/name \"John Doe\"})
+        fetch)
+    ```")
+  (where ^Query [query clauses]
+    "Adds clauses to the query.
+
+    `clauses` is a map where keys represent fields, and values impose restrictions on corresponding fields:
+
+    - a scalar value imposes the field value to be equal to the scalar;
+    - a vector of scalar values imposes equality to one of the values in the vector;
+    - a vector with a leading keyword (or symbol) forms the predicate for testing the field value.
+
+    A key-value pair constructs a predicate. All predicates of the `clauses` map get conjuncted (combined using `and` operator).
+
+    There are two special keys `'or` and `'and` which take a nested `clauses` map as the value and make that map
+    to be combined using corresponding operator.
+
+    The `clauses` are conjuncted with the previously existing clauses of the `query`.
+
+    Example:
+
+    ```
+    (-> users-query
+        (where {:user/role \"admin\"
+                :user/login ['ilike \"a%\"]})
+        fetch)
+    ```")
+  (order ^Query [query order]
+    "Adds or replaces the order of records of the query result.
+
+    `order` is a vector of ASC-ordered fields or a map of fields to
+    ordering direction (`:asc` or `:desc`).
+
+    Examples:
+
+    ```
+    (-> users-query
+        (order [:name :id])
+        fetch)
+
+    (-> employees-query
+        (order {:salary :desc})
+        fetch)
+    ```")
+  (skip ^Query [query amount]
+    "Skips specified amount of records of the result.
+
+    Example:
+
+    ```
+    (-> users-query
+        (skip (* page page-size))
+        (limit page-size)
+        fetch)
+    ```")
+  (limit ^Query [query amount]
+    "Limits the max number of records in the result.
+
+    Example:
+
+    ```
+    (-> users-query
+        (skip (* page page-size))
+        (limit page-size)
+        fetch)
+    ```")
+  (fetch [query] [query fields]
+    "Fetches the data that comply to the query.
+
+    If `fields` specified, only those fields will be fetched.
+
+    Example:
+
+    ```
+    (-> users-query
+        (where {:role \"admin\"})
+        (skip (* page page-size))
+        (limit page-size)
+        (fetch [:id :login]))
+    ```")
+  (fetch-count ^Long [query]
+    "Fetches the amount of records for the given query.
+
+    Example:
+
+    ```
+    (defn get-users-page [page page-size]
+      {:page page
+       :size page-size
+       :total (fetch-count active-users-query)
+       :items (-> active-users-query
+                  (skip (* page page-size))
+                  (limit page-size)
+                  fetch)})
+    ```"))
 
 (defprotocol Entity
   "A persistent entity."
